1888a0be1ec14f354846eef767c646fcf5d8ae52
asma-ad <asmaaderghalmoocs@gmail.com>
2024-10-20 16:54:59 +0100

    Problem1_print_your_name.cpp

---
+++ ProAdv/C_C++/Problem1_print_your_name.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/*
+    Write a program to print your name on screen
+*/
+
+void printName(string name)
+{
+    cout << "Your name is: " << name << endl;
+}
+
+int main()
+{
+    printName("Alex");
+    return 0;
+}
