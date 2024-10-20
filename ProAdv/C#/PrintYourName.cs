c8fc36aa872b829f0a68e061fba86b70fe5319e1
asma-ad <asmaaderghalmoocs@gmail.com>
2024-10-20 15:20:59 +0100

    PrintYourName.cs

---
+++ ProAdv/C#/PrintYourName.cs
@@ -0,0 +1,11 @@
+// Write a program to print your name on the screen.
+
+using System;
+
+public class ProAdv
+{
+    static void PrintName(string name)
+    {
+        Console.WriteLine($"Your name is : {name}");
+    }
+}
