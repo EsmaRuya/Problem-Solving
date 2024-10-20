d62668fdcd697d71621774fe31314edd2c713af6
asma-ad <asmaaderghalmoocs@gmail.com>
2024-10-20 16:08:41 +0100

    FullName.cs

---
+++ ProAdv/C#/FullName.cs
@@ -0,0 +1,39 @@
+// Write a program to ask the user to enter his first name and last name.
+// Then print the full name on the screen.
+using System;
+public class ProAdv
+{
+    struct stInfo 
+    {
+      public string firstName;
+      public string lastName;
+    };
+    
+    static stInfo ReadInfo()
+    {
+        stInfo info;
+        Console.Write("Enter your first name: ");
+        info.firstName = Console.ReadLine();
+       
+        Console.Write("Enter your last name: ");
+        info.lastName = Console.ReadLine();
+        
+        return info;
+    }
+    
+    static string fullName(stInfo info)
+    {
+        return (info.firstName + " " + info.lastName);
+    }
+    
+   static void PrintFullName(string fullName)
+    {
+            Console.WriteLine(fullName);
+    }
+    
+    
+    public static void Main(string[] args)
+    {
+        PrintFullName(fullName(ReadInfo()));
+    }
+}
