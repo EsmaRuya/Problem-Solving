8d2a95c50fbd415158cb31beb55963d1124c6e87
asma-ad <asmaaderghalmoocs@gmail.com>
2024-10-20 16:57:41 +0100

    Prblm1-10

---
+++ ProAdv/C_C++/Problem2_read_name_and_print_it.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*
+Write a program to ask the user to enter their name and print it name on the screen
+*/
+
+string readName()
+{
+    string name;
+    cout << "Please enter your name: ";
+    getline(cin, name);
+    return name;
+}
+
+void printName(string name)
+{
+    cout << "Welcome " << name << endl;
+}
+
+int main()
+{
+    printName(readName());
+    return 0;
+}
---
+++ ProAdv/C_C++/Problem3_check_odd_even.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+/*
+Write a program to ask the user for a number,
+then print "Odd" if it's odd, or "Even" if it's even.
+*/
+
+int readNumber()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    return number;
+}
+
+string checkNumberType(int number)
+{
+
+    return (number % 2 == 0) ? "Even" : "Odd";
+}
+
+int main()
+{
+    cout << checkNumberType(readNumber());
+    return 0;
+}
