a9001485987bfc7bee23f4f28ed071e0bb2f1c98
asma-ad <asmaaderghalmoocs@gmail.com>
2025-05-24 20:24:35 +0100

    Problem66_All_words_from_AAA_to_ZZZ

---
+++ ProAdv/C_C++/Problem66_All_words_from_AAA_to_ZZZ.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+Write a program to all words from AAA to ZZZ
+input : 3
+AAA
+AAB
+AAC
+...
+ZZZ
+*/
+
+void printWordsFromAAAtoZZZ()
+{
+    string word = "";
+    
+       for (int i = 65; i <= 90; i++)
+       {
+           for (int j = 65; j <= 90; j++)
+           {
+               for (int k = 65; k <= 90; k++)
+               {
+                   word = word + char (i);
+                   word = word + char (j);
+                   word = word + char (k);
+                   
+                   cout << word << endl;
+                   
+                   word = "";
+               }
+           }
+       }
+}
+
+int main()
+{
+    printWordsFromAAAtoZZZ();
+    return 0;
+}
