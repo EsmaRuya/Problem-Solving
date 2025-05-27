06a8028fb412a620a52ad18f6f6b8322a8d2a0d8
asma-ad <asmaaderghalmoocs@gmail.com>
2025-05-27 07:41:01 +0100

    Problem86_Add_array_elements_simi_dynamic

---
+++ ProAdv/C_C++/Problem86_Add_array_elements_simi_dynamic.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+/*
+Write a program to dynamically read numbers and save them in an array max size of array is 100, allocate simi-dynamic array length;
+*/
+
+int readNumber()
+{
+    int number = 0;
+    cout << "Enter a number: ";
+    cin >> number;
+    return number;
+}
+
+void addNumberToArray(int arr[], int &length, int number)
+{
+    length++;
+    arr[length - 1] = number;
+}
+
+void inputUserNumbersInArray(int arr[], int &length)
+{
+    bool add_more = true;
+    do
+    {
+        addNumberToArray(arr, length, readNumber());
+        cout << "Do you want to add more numbers?\nIf yes enter 1, if no enter 0\n";
+        cin >> add_more;
+    } while (add_more);
+}
+
+void printArray(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[100], length = 0;
+    inputUserNumbersInArray(arr, length);
+    cout << "\nArray length: " << length;
+    cout << "\nArray elements: ";
+    printArray(arr, length);
+
+    return 0;
+}
