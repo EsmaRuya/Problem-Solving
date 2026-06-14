7ed6c53d34833c6d5fae8e8b17238d54d01aa8ba
EsmaRuya <EsmaRuya@email>
2026-06-14 12:42:32 +0100

    007_Practice - Person Data 2

---
+++ WinForms/007_Practice/Form1.cs
@@ -0,0 +1,272 @@
+using System.IO;
+using System.Linq.Expressions;
+
+namespace _007_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string _Path = Path.Combine(Application.StartupPath, "Data.txt");
+        string _ImagePath;
+        private bool _isPersonIdExist(string ID)
+        {
+            StreamReader sr = new StreamReader("Data.txt");
+            string strLine;
+            while ((strLine = sr.ReadLine()) != null)
+            {
+                string[] arrLine = strLine.Split(';');
+                if (arrLine[0] == ID)
+                {
+                    sr.Close();
+                    return true;
+                }
+            }
+            sr.Close();
+            return false;
+            //  if (strLine.Contains(txtBox_ID.Text + ";"))
+            // return true;
+        }
+
+        private string[] _GetPersonData(string ID)
+        {
+            StreamReader sr = new StreamReader(_Path);
+            string strLine;
+            string[] arrLine;
+
+            while ((strLine = sr.ReadLine()) != null)
+            {
+                arrLine = strLine.Split(';');
+                if (arrLine[0] == ID)
+                {
+                    sr.Close();
+                    return arrLine;
+                }
+            }
+            sr.Close();
+            return null;
+        }
+        private string _GetAllData()
+        {
+            //  return File.ReadAllText("Data.txt");
+            using StreamReader sr = new StreamReader(_Path);
+            return sr.ReadToEnd();
+        }
+
+        private void _EmptyAllTextBoxes()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                    c.Text = "";
+            }
+        }
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtBox_ID.Text.Trim() == "" || txtBox_Name.Text.Trim() == "" || txtBox_Address.Text.Trim() == "")
+                {
+                    MessageBox.Show("Fields are emty! Enter all data.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                if (_isPersonIdExist(txtBox_ID.Text))
+                {
+                    MessageBox.Show("This ID is exist, please try new one!", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtBox_ID.Focus();
+                }
+                else
+                {
+                    StreamWriter sw = new StreamWriter(_Path, true);
+
+                    string strPersonData = txtBox_ID.Text + ";" +
+                                 txtBox_Name.Text + ";" +
+                                 txtBox_Address.Text;
+                    sw.WriteLine(strPersonData);
+                    sw.Close();
+
+                    if (!Directory.Exists("imgs"))
+                        Directory.CreateDirectory("imgs");
+                    picPerson.Image.Save($"imgs/{txtBox_ID.Text}.jpg");
+
+                    MessageBox.Show("Person is added successfully", "Add person", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    _EmptyAllTextBoxes();
+                    picPerson.Image = new PictureBox().Image;
+                    txtBox_ID.Focus();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtBox_ID.Text.Trim() != "")
+                {
+                    string[] arrData = _GetPersonData(txtBox_ID.Text);
+                    if (arrData != null)
+                    {
+                        txtBox_ID.Text = arrData[0];
+                        txtBox_Name.Text = arrData[1];
+                        txtBox_Address.Text = arrData[2];
+
+                        _ImagePath = Path.Combine("imgs", $"{arrData[0]}.jpg");
+                        if (File.Exists(_ImagePath))
+                        {
+                            picPerson.Image = Image.FromFile(_ImagePath);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("This person ID doesn't exist!", "Not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtBox_ID.Focus();
+                        txtBox_ID.SelectAll();
+                        txtBox_Name.Text = "";
+                        txtBox_Address.Text = "";
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Please enter ID to search for the person!", " lack of info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtBox_ID.Focus();
+                    txtBox_ID.SelectAll();
+                    txtBox_Name.Text = "";
+                    txtBox_Address.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(_Path))
+            {
+                MessageBox.Show("No data yet! Please add new person to create a file and show data again.", "No file exist", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            Form frm = new Form();
+            TextBox txtBox = new TextBox();
+
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            frm.Font = new Font(this.Font.Name, 18);
+            frm.Icon = this.Icon;
+            frm.Size = this.Size * 2;
+            frm.Text = "All data";
+
+            txtBox.Multiline = true;
+            txtBox.Dock = DockStyle.Fill;
+            txtBox.Enabled = false;
+
+            frm.Controls.Add(txtBox);
+            try
+            {
+                txtBox.Text = _GetAllData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            frm.ShowDialog();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // this.Close();
+            Application.Exit();
+        }
+
+        private void btnAddPhoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Images|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            openFile.Title = "Select Photo";
+            openFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                picPerson.Image = Image.FromFile(openFile.FileName);
+            }
+        }
+
+        private void btnShowAllWithPictures_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(_Path))
+            {
+                MessageBox.Show("No data yet! Please add new person to create a file and show data again.", "No file exist", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            Form frm = new Form();
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            frm.Font = new Font(this.Font.Name, 14);
+            frm.Icon = this.Icon;
+            frm.Size = this.Size * 2;
+            frm.AutoScroll = true;
+            frm.Text = "All data with images";
+            frm.AutoScroll = true;
+
+            try
+            {
+                StreamReader sr = new StreamReader(_Path);
+                string strLine = "";
+                string imagePath = "";
+                int top = 10;
+
+                do
+                {
+                    strLine = sr.ReadLine();
+                    if(strLine != null)
+                    {
+                        TextBox txt = new TextBox();
+                        PictureBox pic = new PictureBox();
+
+                        txt.Width = 300;
+                        txt.Top = top;
+                        txt.Left = 50;
+                        txt.Text = strLine;
+
+                        pic.BorderStyle = BorderStyle.FixedSingle;
+                        pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pic.Size = new Size(100,100);
+                        pic.Top = top;
+                        pic.Left = 400;
+
+                        imagePath = $"imgs/{strLine.Split(';')[0]}.jpg";
+                        if(File.Exists(imagePath))
+                        {
+                            pic.Image = Image.FromFile(imagePath);
+                        }
+                        else
+                        {
+                            pic.Image = Properties.Resources.person_40; 
+                        }
+                        top += 110;
+ 
+                        frm.Controls.Add(txt);
+                        frm.Controls.Add(pic);
+                    }
+                } while(strLine != null);
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            frm.ShowDialog();
+        }
+    }
+}
